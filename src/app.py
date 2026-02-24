35e7c827feb101174773ee6a44c34724bce0345c
kittyiv <kittyiv@gmail.com>
2026-02-24 09:02:14 -0600

    test

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little doggie!! <3',
+        'message': 'I\d rahter have a bottle in front of me, than to have a frontal lobotoy <3',
         'deployed_on': 'kubernetes'
     })
 
